8aeb8c1d87ef7dc7a4503481977355a09b068225
Pedro Vitoreti <133826934+PHV00@users.noreply.github.com>
2025-03-23 18:44:12 -0300

    feat: First screen (home) is done

--- noter/app/components/NavBar.tsx
+++ noter/app/components/NavBar.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import * as React from "react";
 
 export default function Navbar() {
+  const router = useRouter();
+
   return (
     <div className="w-full h-1/12 border-b-2">
-        <div className="pl-12 pr-12 w-full h-full flex items-center justify-between">
+        <div className="px-12 w-full h-full flex items-center justify-between">
             <div className="w-[5vw]">
                 <h1 className="italic font-bold text-2xl">Noter</h1>
             </div>
             <div className="w-[5vw]">
-                <button className="w-full rounded-md bg-amber-200 p-1">Sing In</button>
+                <button 
+                  onClick={()=>{router.push("pages/eanotation")}}
+                  className="w-full rounded-md bg-amber-200 p-1">
+                  Sing In
+                </button>
             </div>
         </div>
     </div>
--- noter/app/pages/Home/page.tsx
+++ noter/app/pages/Home/page.tsx
@@ -5,12 +5,13 @@ import Image from "next/image";
 import LittleWriter from "../../../public/LittleWriter.svg";
 
 export default function Home(){
+    
     return (
         <div className="w-screen h-screen ">
             <Navbar></Navbar>
-            <div className="mt-[5vw] flex flex-row justify-center items-center">
+            <div className=" mt-[5vw] flex flex-row justify-center items-center">
 
-                <div className="w-1/2 h-full pr-3 flex flex-row justify-center">
+                <div className="w-1/2 h-full flex flex-row justify-center">
                     <Image
                     alt="little noter boy "
                     quality={100}
@@ -21,14 +22,12 @@ export default function Home(){
                 </div>
 
                 <div className="w-1/2 h-full">
-                    <h1 className="text-center text-5xl font-bold">
+                    <h1 className="text-center text-6xl font-bold">
                         Wellcome to Noter
                     </h1>
-               
-                    <h2 className="mt-[5vw] text-4xl w-11/12">
+                    <h2 className="mt-[5vw] text-center text-4xl w-11/12">
                         Your notes anywhere and anytime, click on Sign in to get started right now.    
                     </h2>
-               
                     {/* <div className="w-1/3 h-[3vw] justify-center rounded-md border-1 border-solid border-black flex flex-row">
                         <input className="w-3/4 h-full pl-[1vw] focus:outline-none" placeholder="Try write something ..."></input>
                         <svg className="w-1/4 h-1/4" ></svg>
